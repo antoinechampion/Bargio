7e46bca028789b647677e3343ec38ee602915010
Alexandre Orain <orain.alex@gmail.com>
2018-10-24 00:15:38 +0200

    ajout mod mdp + definir mdp

---
+++ Code statique/Définir mdp/script.js
@@ -0,0 +1,8 @@
+function ShowHideDiv() {
+    "use strict";
+    if (document.getElementById('MDPoptionnel').checked) {
+        document.getElementById('txtMDPopt').style.display = 'none';
+    } else {
+        document.getElementById('txtMDPopt').style.display = 'block';
+    }
+}
