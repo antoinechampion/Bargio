1e9fc481b668bd319dc5a6d34490f6b56044d881
Alexandre Orain <orain.alex@gmail.com>
2018-10-24 16:25:40 +0200

    logos

--- Code statique/Page de connexion/script.js
+++ Code statique/Page de connexion/script.js
@@ -2,7 +2,9 @@ function ShowHideDiv() {
     "use strict";
     if (document.getElementById('MDPoptionnel').checked) {
         document.getElementById('txtMDPopt').style.display = 'none';
+        
     } else {
         document.getElementById('txtMDPopt').style.display = 'block';
+        
     }
 }
