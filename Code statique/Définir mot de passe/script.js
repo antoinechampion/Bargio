49f874e68a5325825c8efc0480b1a9a13ae4b8f3
Alexandre Orain <orain.alex@gmail.com>
2018-11-05 16:05:47 +0100

    mod page definir mdp

--- Code statique/Définir mot de passe/script.js
+++ Code statique/Définir mot de passe/script.js
@@ -1,4 +1,9 @@
-function ShowHideDiv() {
+function ShowDiv() {
     "use strict";
     document.getElementById('conf').style.display = "block";
+}
+
+function HideDiv() {
+    "use strict";
+    document.getElementById('conf').style.display = "none";
 }
