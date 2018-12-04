1b2db52a3ab2f4c9534096a67be4984e3e06e494
Champion Antoine <antoine-_-champion@hotmail.fr>
2018-12-04 13:49:35 +0100

    correction bug chartjs

--- Code statique/Accueil mobile/script.js
+++ Code statique/Accueil mobile/script.js
@@ -1,40 +1,44 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
+$(document).ready(function () {
+	
+	/* Set the width of the side navigation to 250px */
+	function openNav() {
+		document.getElementById("mySidenav").style.width = "250px";
+	}
 
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
+	/* Set the width of the side navigation to 0 */
+	function closeNav() {
+		document.getElementById("mySidenav").style.width = "0";
+	}
 
-function ShowHideDiv() {
-    "use strict";
-    if (document.getElementById('MDPoptionnel').checked) {
-        document.getElementById('txtMDPopt').style.display = 'none';
-        
-    } else {
-        document.getElementById('txtMDPopt').style.display = 'block';
-        
-    }
-}
+	function ShowHideDiv() {
+		"use strict";
+		if (document.getElementById('MDPoptionnel').checked) {
+			document.getElementById('txtMDPopt').style.display = 'none';
+			
+		} else {
+			document.getElementById('txtMDPopt').style.display = 'block';
+			
+		}
+	}
 
-new Chart(document.getElementById("doughnut-chart"), {
-    type: 'doughnut',
-    data: {
-        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-        datasets: [
-            {
-                label: "Population (millions)",
-                backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9" , "#c45850"],
-                data: [2478, 5267, 734, 784, 433]
-            }
-        ]
-    },
-    options: {
-        title: {
-            display: true,
-            text: 'Predicted world population (millions) in 2050'
-        }
-    }
+	new Chart(document.getElementById("doughnut-chart").getContext('2d')
+	, {
+		type: 'doughnut',
+		data: {
+			labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+			datasets: [
+				{
+					label: "Population (millions)",
+					backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9" , "#c45850"],
+					data: [2478, 5267, 734, 784, 433]
+				}
+			]
+		},
+		options: {
+			title: {
+				display: true,
+				text: 'Predicted world population (millions) in 2050'
+			}
+		}
+	});
 });
