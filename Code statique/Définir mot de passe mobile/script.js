543c8e1f9db18fa140a20846b748abae5b9971e2
Antoine Champion <antoine.chapion@outlook.fr>
2021-01-23 12:46:07 +0100

    Clean repo

--- Code statique/Définir mot de passe mobile/script.js
+++
@@ -1,9 +0,0 @@
-function ShowDiv() {
-    "use strict";
-    document.getElementById('conf').style.display = "block";
-}
-
-function HideDiv() {
-    "use strict";
-    document.getElementById('conf').style.display = "none";
-}
